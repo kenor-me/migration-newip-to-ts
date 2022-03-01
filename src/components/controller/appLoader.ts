e4ae58fc67ce10b52c532d4fbb5e80e494344a73
Katherine Kanareikina <katyabrizitskaya@mail.ru>
2022-03-01 08:34:53 +0300

    feat: changed styles

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.herokuapp.com/', {
       apiKey: 'af2009b2d015486d84ca91b797c9ff78', // получите свой ключ https://newsapi.org/
     });
   }
